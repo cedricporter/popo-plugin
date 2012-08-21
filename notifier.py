5decbdfb862f9a27f2dd61aee7fd42af65addea0
Cedric Porter <cedricporter@gmail.com>
2012-08-21 19:07:09 +0800

    support all linux with libnotify. Thanks to Lin Yuyu.

--- notifier.py
+++ notifier.py
@@ -7,26 +7,16 @@
 
 import tornado.httpserver
 import tornado.ioloop
-import tornado.web
-try:
-    import pynotify
-    def ShowNotification(title, msg):
-        n = pynotify.Notification ("主人，您有了新的泡泡消息", title)
-        n.show()
-
-except:
-    def ShowNotification(title, msg):
-        knotify = dbus.SessionBus().et_object("org.kde.knotify", "/Notify")
-        id = knotify.event("warning", "kde", [], title, msg, [], [], 0, 0, dbus_interface="org.kde.KNotify")
-        time.sleep(3)
-        knotify.closeNotification(id)
+import tornado.web 
+from gi.repository import Notify
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         title = self.get_argument('title')
-        # print title
-        ShowNotification("主人，您有了新的泡泡消息", title)
+        print title
+        Hello = Notify.Notification.new("主人，您有了新的泡泡消息", title, "dialog-information")
+        Hello.show()
 
 settings = {
     'debug': True,
@@ -40,7 +30,7 @@ application = tornado.web.Application(
 
 
 if __name__ == "__main__":
-    pynotify.init("popo")
+    Notify.init("popo")
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(34567)
     tornado.ioloop.IOLoop.instance().start()
